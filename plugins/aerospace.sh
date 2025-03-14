f9489e414fc6bf1de1920ecfe68e130e478487e0
GnussonNet <admin@gnusson.net>
2025-03-14 22:26:54 +0100

    Changed corner radius

--- plugins/aerospace.sh
+++ plugins/aerospace.sh
@@ -25,7 +25,7 @@ case "$1" in
 esac
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set "$NAME" background.drawing=on icon="$ICON" icon.padding_left="$ICON_PADDING" icon.padding_right="$ICON_PADDING" background.color=0xffff7b72 background.corner_radius=3 background.height=20 color=0xff0d1117 
+  sketchybar --set "$NAME" background.drawing=on icon="$ICON" icon.padding_left=10 icon.padding_right="$ICON_PADDING" background.color=0xffff7b72 background.corner_radius=0 background.height=20 color=0xff0d1117
 else
-  sketchybar --set "$NAME" background.drawing=off icon="$ICON" icon.padding_left="$ICON_PADDING" icon.padding_right="$ICON_PADDING" background.color=0xff58a6ff background.corner_radius=3 background.height=20 color=0xff24282e
+  sketchybar --set "$NAME" background.drawing=off icon="$ICON" icon.padding_left=10 icon.padding_right="$ICON_PADDING" background.color=0xff58a6ff background.corner_radius=0 background.height=20 color=0xff24282e
 fi
